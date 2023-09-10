19d95652df2122ffd0e44e8672192796837ab81e
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:41:09 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from tensorflow.keras.datasets import mnist
 
 # Step 1: Dataset Collection
+
+
 def load_dataset():
     # Load your real and deepfake video datasets here
     # You can use FaceForensics++ or other datasets
     return real_videos, deepfake_videos
 
 # Step 2: Preprocessing
+
+
 def preprocess_frames(frames):
     features = []
     for frame in frames:
@@ -23,9 +27,11 @@ def preprocess_frames(frames):
         features.append(feature)
     return np.array(features)
 
+
 def extract_features(frame):
     # Implement your custom feature extraction process here
     return feature
+
 
 def preprocess_audio(audio):
     features = []
@@ -36,27 +42,31 @@ def preprocess_audio(audio):
     return np.array(features)
 
 # Step 3: Adversarial Autoencoder Training
+
+
 def build_autoencoder(input_shape):
     inputs = Input(shape=input_shape)
-    
+
     # Encoder
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     x = MaxPooling2D((2, 2), padding='same')(x)
     x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    
+
     # Decoder
     x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    outputs = Conv2D(input_shape[2], (3, 3), activation='sigmoid', padding='same')(x)
-    
+    outputs = Conv2D(input_shape[2], (3, 3),
+                     activation='sigmoid', padding='same')(x)
+
     return Model(inputs=inputs, outputs=outputs)
+
 
 def build_discriminator(input_shape):
     inputs = Input(shape=input_shape)
-    
+
     # Discriminator
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     x = MaxPooling2D((2, 2), padding='same')(x)
@@ -64,8 +74,9 @@ def build_discriminator(input_shape):
     x = MaxPooling2D((2, 2), padding='same')(x)
     x = Flatten()(x)
     outputs = Dense(1, activation='sigmoid')(x)
-    
+
     return Model(inputs=inputs, outputs=outputs)
+
 
 def train_autoencoder(real_videos, deepfake_videos):
     # Preprocess real and deepfake videos
@@ -73,52 +84,61 @@ def train_autoencoder(real_videos, deepfake_videos):
     deepfake_frames = preprocess_frames(deepfake_videos)
 
     # Split training and validation data
-    x_train, x_val, _, _ = train_test_split(np.concatenate((real_frames, deepfake_frames)), np.zeros(len(real_frames) + len(deepfake_frames)), test_size=0.2)
-    
+    x_train, x_val, _, _ = train_test_split(np.concatenate((real_frames, deepfake_frames)), np.zeros(
+        len(real_frames) + len(deepfake_frames)), test_size=0.2)
+
     # Normalize input data
     x_train = x_train / 255.0
     x_val = x_val / 255.0
-    
+
     # Build and compile the autoencoder model
     input_shape = x_train[0].shape
     autoencoder = build_autoencoder(input_shape)
     autoencoder.compile(optimizer='adam', loss='mse')
-    
+
     # Train the autoencoder
-    autoencoder.fit(x_train, x_train, validation_data=(x_val, x_val), epochs=10, batch_size=32)
+    autoencoder.fit(x_train, x_train, validation_data=(
+        x_val, x_val), epochs=10, batch_size=32)
 
     # Extract encoder and decoder models
-    encoder = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer(index=1).output)
+    encoder = Model(inputs=autoencoder.input,
+                    outputs=autoencoder.get_layer(index=1).output)
     decoder = Model(inputs=autoencoder.input, outputs=autoencoder.output)
-    
+
     return encoder, decoder
+
 
 def train_discriminator(encoder, real_frames, deepfake_frames):
     # Preprocess real and deepfake frames
     real_encoded = encoder.predict(preprocess_frames(real_frames))
     deepfake_encoded = encoder.predict(preprocess_frames(deepfake_frames))
-    
+
     # Create labels for real and deepfake frames
-    labels = np.concatenate((np.ones(len(real_encoded)), np.zeros(len(deepfake_encoded))))
-    
+    labels = np.concatenate(
+        (np.ones(len(real_encoded)), np.zeros(len(deepfake_encoded))))
+
     # Split training and validation data
     x_train, x_val, y_train, y_val = train_test_split(
         np.concatenate((real_encoded, deepfake_encoded)),
         labels,
         test_size=0.2
     )
-    
+
     # Build and compile the discriminator model
     input_shape = x_train[0].shape
     discriminator = build_discriminator(input_shape)
-    discriminator.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    
+    discriminator.compile(
+        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
     # Train the discriminator
-    discriminator.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=10, batch_size=32)
-    
+    discriminator.fit(x_train, y_train, validation_data=(
+        x_val, y_val), epochs=10, batch_size=32)
+
     return discriminator
 
 # Step 4: Evaluation and Validation
+
+
 def evaluate_model(encoder, discriminator, test_real_videos, test_deepfake_videos):
     # Preprocess test videos
     test_real_frames = preprocess_frames(test_real_videos)
@@ -127,57 +147,65 @@ def evaluate_model(encoder, discriminator, test_real_videos, test_deepfake_video
     # Encode test frames
     test_real_encoded = encoder.predict(test_real_frames)
     test_deepfake_encoded = encoder.predict(test_deepfake_frames)
-    
+
     # Create labels for test videos
     test_real_labels = np.ones(len(test_real_encoded))
     test_deepfake_labels = np.zeros(len(test_deepfake_encoded))
-    
+
     # Combine test data and labels
     x_test = np.concatenate((test_real_encoded, test_deepfake_encoded))
     y_test = np.concatenate((test_real_labels, test_deepfake_labels))
-    
+
     # Evaluate the model
     y_pred = discriminator.predict(x_test)
     y_pred_labels = (y_pred > 0.5).astype(int)
-    
+
     accuracy = accuracy_score(y_test, y_pred_labels)
     precision = precision_score(y_test, y_pred_labels)
     recall = recall_score(y_test, y_pred_labels)
     f1 = f1_score(y_test, y_pred_labels)
-    
+
     return accuracy, precision, recall, f1
 
 # Step 5: Deployment
+
+
 def predict_deepfake(encoder, discriminator, video):
     # Preprocess video frames
     frames = preprocess_frames(video)
-    
+
     # Encode frames
     encoded_frames = encoder.predict(frames)
-    
+
     # Predict probability of being deepfake
     predictions = discriminator.predict(encoded_frames)
-    
+
     return np.mean(predictions)
+
 
 # Load the dataset
 real_videos, deepfake_videos = load_dataset()
 
 # Split dataset into training and testing data
-train_real_videos, test_real_videos = train_test_split(real_videos, test_size=0.2)
-train_deepfake_videos, test_deepfake_videos = train_test_split(deepfake_videos, test_size=0.2)
+train_real_videos, test_real_videos = train_test_split(
+    real_videos, test_size=0.2)
+train_deepfake_videos, test_deepfake_videos = train_test_split(
+    deepfake_videos, test_size=0.2)
 
 # Train the autoencoder
 encoder, decoder = train_autoencoder(train_real_videos, train_deepfake_videos)
 
 # Train the discriminator
-discriminator = train_discriminator(encoder, train_real_videos, train_deepfake_videos)
+discriminator = train_discriminator(
+    encoder, train_real_videos, train_deepfake_videos)
 
 # Evaluate the model
-accuracy, precision, recall, f1 = evaluate_model(encoder, discriminator, test_real_videos, test_deepfake_videos)
+accuracy, precision, recall, f1 = evaluate_model(
+    encoder, discriminator, test_real_videos, test_deepfake_videos)
 
 # Deploy the deepfake detection system
-input_video = cv2.VideoCapture('input_video.mp4')  # Update with your video file
+# Update with your video file
+input_video = cv2.VideoCapture('input_video.mp4')
 
 frames = []
 
@@ -186,7 +214,7 @@ while True:
     if not ret:
         break
     frames.append(frame)
-    
+
 video_confidence = predict_deepfake(encoder, discriminator, frames)
 
 print("Accuracy:", accuracy)
